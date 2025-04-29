ab2fcf81ba4b5496ff526c7809dae74acace94c3
sd2013 <57208396+sd2013@users.noreply.github.com>
2025-04-29 12:44:54 +0530

    Add files via upload

---
+++ src/main/java/com/example/headerApp/HeaderAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.headerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeaderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeaderAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/headerApp/HeaderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.headerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeaderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
